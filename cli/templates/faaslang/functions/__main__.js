59d3857a81dbbf2650516face090f522c68a710b
Jacob <jacoblee93@gmail.com>
2017-06-28 17:55:27 -0700

    Adds the lib package preinstalled for newly created services

--- cli/templates/faaslang/functions/__main__.js
+++ cli/templates/faaslang/functions/__main__.js
@@ -1,4 +1,11 @@
 /**
+ * Use lib to call other functions from StdLib's registry, available at
+ * (https://stdlib.com/search). For example, try lib.utils.reflect(callback) or
+ * lib.utils.sms(toNumber, message, callback).
+ */
+const lib = require('lib');
+
+/**
 * A basic Hello World function
 * @param {string} name Who you're saying hello to
 * @returns {string}
