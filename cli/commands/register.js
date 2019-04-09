1b4ed9429f5094cb569bd72ac6ab0d8965b5055a
Jacob <jacoblee93@gmail.com>
2019-04-09 15:33:38 -0700

    Recommend lib init instead of lib login for new accounts

--- cli/commands/register.js
+++ cli/commands/register.js
@@ -26,7 +26,7 @@ class RegisterCommand extends Command {
     console.log(`Creating new accounts through the terminal has been ${chalk.bold('deprecated')}.`);
     console.log(`Please create an account by going to "https://stdlib.com/".`);
     console.log();
-    console.log(`Once complete, use ${chalk.bold('lib login')} to log in and use the CLI.`);
+    console.log(`Once complete, use ${chalk.bold('lib init')} to initialize your workspace and use the CLI.`);
     console.log();
 
   }
