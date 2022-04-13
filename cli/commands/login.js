20f88bd008b07f916b126b195281ec6a242f3540
Jacob Lee <jacoblee@Jacobs-MacBook-Pro.local>
2022-04-12 18:37:57 -0700

    Adds support for two factor authentication

--- cli/commands/login.js
+++ cli/commands/login.js
@@ -55,6 +55,40 @@ class LoginCommand extends Command {
       message: 'Password',
     });
 
+    let resource = new APIResource(host, port);
+
+    let setAccessToken = (accessToken, cb) => {
+
+      config.set('ACCESS_TOKEN', accessToken);
+      config.set('ACTIVE_LIBRARY_TOKEN', '');
+      config.unset('LIBRARY_TOKENS');
+      config.write();
+
+      console.log();
+      console.log(chalk.bold.green('Logged in successfully!') + ' Retrieving default Identity Token (API Key)...');
+
+      resource.authorize(config.get('ACCESS_TOKEN'));
+      resource.request('/v1/library_tokens').index({default: true}, (err, response) => {
+
+        if (err) {
+          return cb(err);
+        }
+
+        let tokens = (response && response.data) || [];
+
+        if (!tokens.length) {
+          console.log('Logged in, but could not retrieve default Identity Token.');
+        } else {
+          config.save('ACTIVE_LIBRARY_TOKEN', tokens[0].token);
+          console.log(`Active Identity Token (API Key) set to: ${chalk.bold(tokens[0].label)}`);
+        }
+
+        console.log();
+        return cb();
+
+      });
+    }
+
     let loopCb = async (err) => {
 
       err && errorLog(err);
@@ -64,9 +98,7 @@ class LoginCommand extends Command {
       email = promptResult.email || email;
       password = promptResult.password || password;
 
-      let resource = new APIResource(host, port);
-
-      resource.request('v1/access_tokens').create({}, {grant_type: 'password', username: email, password: password}, (err, response) => {
+      resource.request('v1/login').create({}, {grant_type: 'password', username: email, password: password}, (err, response) => {
 
         if (err) {
           questions.filter(q => q.name === 'email').forEach(q => q.default = email);
@@ -74,35 +106,34 @@ class LoginCommand extends Command {
           return loopCb(err);
         }
 
-        config.set('ACCESS_TOKEN', response.data[0].access_token);
-        config.set('ACTIVE_LIBRARY_TOKEN', '');
-        config.unset('LIBRARY_TOKENS');
-        config.write();
-
-        console.log();
-        console.log(chalk.bold.green('Logged in successfully!') + ' Retrieving default Identity Token (API Key)...');
-
-        resource.authorize(config.get('ACCESS_TOKEN'));
-        resource.request('/v1/library_tokens').index({default: true}, (err, response) => {
-
-          if (err) {
-            return callback(err);
-          }
-
-          let tokens = (response && response.data) || [];
-
-          if (!tokens.length) {
-            console.log('Logged in, but could not retrieve default Identity Token.');
-          } else {
-            config.save('ACTIVE_LIBRARY_TOKEN', tokens[0].token);
-            console.log(`Active Identity Token (API Key) set to: ${chalk.bold(tokens[0].label)}`);
-          }
-
+        if (!!response.data[0].factor_identifier) {
           console.log();
-          return callback();
-
-
-        });
+          console.log(`Your account has two-factor authentication enabled. Please enter a valid authentication code to finish logging in.`);
+          console.log();
+          inquirer.prompt({
+            name: 'verificationCode',
+            type: 'input',
+            default: '',
+            message: 'Verification Code',
+          }).then((promptResult) => {
+            resource.request('v1/login').create({}, {
+              grant_type: 'password',
+              username: email,
+              password: password,
+              factor_verification_check_sid: response.data[0].sid,
+              factor_verification_code: promptResult.verificationCode,
+            }, (err, response) => {
+              if (err) {
+                questions.filter(q => q.name === 'email').forEach(q => q.default = email);
+                password = null;
+                return loopCb(err);
+              }
+              setAccessToken(response.data[0].access_token, callback);
+            });
+          });
+        } else {
+          setAccessToken(response.data[0].access_token, callback);
+        }
 
       });
 
